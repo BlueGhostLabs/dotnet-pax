8d2e82347f2447d96eab56ac9bfd398d63a97ff6
Charles Phillips <cphill11@gmail.com>
2018-10-09 14:13:13 -0400

    Its working

--- src/dotnet-pax/Program.fs
+++ src/dotnet-pax/Program.fs
@@ -1,16 +1,38 @@
 ﻿open System
+open System.IO
 
-type Folder<'a,'b> = Folder of list<'a * Folder<'a,'b>> | File of 'b
+type Folder = File of string | Folder of string * Folder list
 
 // Envs folder: dev.tfvars, qa.tfvars, prod.tfvars
+let envsFolder = Folder ("envs", [File "dev.tfvars"; File "qa.tfvars"; File "prod.tfvars"])
+
 // Service Folder:  Envs folder, main.tf, variables.tf, output.tfvars
+let serviceFolder = Folder("service", [File "main.tf"; File "variables.tf"; File "output.tfvars"; envsFolder])
+
+let infrastructureFolder = Folder("infrastructure", [serviceFolder])
 
 
 type Commands = Init | Service
 
 type SubCommands = New | Update | Delete
 
+
+// Folder -> string
+let rec folderToString folder =
+    match folder with
+    | File fileName -> [fileName]
+    | Folder (name, []) -> [name + "/"]
+    | Folder (name, otherFolders) ->
+        otherFolders |> List.map folderToString |> List.collect (fun s -> s |> List.map (fun x -> name + "/" + x)) 
+
+let createFile (fileName: string) = 
+    System.IO.Directory.CreateDirectory(Path.GetDirectoryName(fileName)) |> ignore
+    System.IO.File.Create(fileName).Close()
+
 [<EntryPoint>]
 let main argv =
+
+    infrastructureFolder |> folderToString |> List.iter createFile
+    
     printfn "Hello World from F#!"
     0 // return an integer exit code
